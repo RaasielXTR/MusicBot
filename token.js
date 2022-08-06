6bb5e88aa8de8f8f6198e39fa7c10197d0df19fd
RaasielXTR <110736177+RaasielXTR@users.noreply.github.com>
2022-08-06 19:35:50 +0300

    Update token.js

--- token.js
+++ token.js
@@ -1,4 +1,4 @@
 module.exports = {
     //rename this file to token.js
-    botToken: "" //YOUR TOKEN HERE
+    botToken: "MTAwMTM2NjE3ODU5MTE1MDExMA.GXMj0p.ZskAQADhibaVM0GB56zynDEeAbzdUhbv3qO8Ho" //YOUR TOKEN HERE
 }
